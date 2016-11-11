9c139731a0a4f191b9c50c6c62cadbcf31d0a577
James Link <jamesmlink@gmail.com>
2016-11-10 19:34:07 -0500

    ran into roadblock, closing out where stuck

---
+++ README.md
@@ -0,0 +1,5 @@
+this is a practice exercise I completed to reinforce concepts learned from Treehouse's full stack javascript track.
+
+Video URL here: https://www.youtube.com/watch?v=PFP0oXNNveg
+
+minimial if any changes besides some extra comments and formatting changes
---
+++ client/app/components/tasks/tasks.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+
+
+@Component({
+	moduleId: module.id,
+  selector: 'tasks',
+  templateUrl: 'tasks.component.html'
+})
+export class TasksComponent {
+	constructor(private taskService: TaskService){
+	
+	}
+}
